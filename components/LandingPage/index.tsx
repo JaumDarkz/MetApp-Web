5d4461ef5d04bda096ae71d186df1f980e65f36f
JaumDarkz <viniciuspkfr@gmail.com>
2022-10-22 13:02:32 -0300

    feat: add Navigation Bar Component

---
+++ components/LandingPage/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss'
+
+const LandingPage = () => {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
+
+export default LandingPage
