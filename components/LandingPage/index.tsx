ef6f07d80bbf9bce6fdebf2d7819607084c09260
JaumDarkz <viniciuspkfr@gmail.com>
2022-10-23 21:41:20 -0300

    feat: add hero section and content section to LP

--- components/LandingPage/index.tsx
+++ components/LandingPage/index.tsx
@@ -1,9 +1,13 @@
 import styles from './styles.module.scss'
 
+import Hero from './components/Hero'
+import Content from './components/Content'
+
 const LandingPage = () => {
   return (
     <div className={styles.container}>
-      
+      <Hero />
+      <Content />
     </div>
   )
 }
